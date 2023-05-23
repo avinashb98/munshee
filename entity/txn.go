6fc6ed5b0d24662593d3fbdf67e73f6e6187b6ab
Avinash Bhardwaj <avinashb97@gmail.com>
2023-05-24 01:39:56 +0530

    disable unwanted code

--- entity/txn.go
+++ entity/txn.go
@@ -1,7 +1,5 @@
 package entity
 
-import "github.com/google/uuid"
-
 type TxnIn struct {
 	Description string   `json:"description"`
 	ToAccount   *string  `json:"to_account"`
@@ -49,45 +47,45 @@ type TxnOut struct {
 
 type TxnOption func(*Txn)
 
-func WithDescription(description string) TxnOption {
-	return func(t *Txn) {
-		t.Description = description
-	}
-}
-
-func WithToAccount(toAccount *string) TxnOption {
-	return func(t *Txn) {
-		t.ToAccount = toAccount
-	}
-}
-
-func WithFromAccount(fromAccount *string) TxnOption {
-	return func(t *Txn) {
-		t.FromAccount = fromAccount
-	}
-}
-
-func WithTags(tags ...Tag) TxnOption {
-	return func(t *Txn) {
-		var tagsStr []string
-		for _, tag := range tags {
-			tagsStr = append(tagsStr, tag.Name)
-		}
-		t.Tags = tagsStr
-	}
-}
-
-func WithAmount(amount float64) TxnOption {
-	return func(t *Txn) {
-		t.Amount = amount
-	}
-}
-
-func NewTxn(userID string, opts ...TxnOption) Txn {
-	txn := Txn{Username: userID, ID: uuid.New().String()}
-
-	for _, opt := range opts {
-		opt(&txn)
-	}
-	return txn
-}
+//func WithDescription(description string) TxnOption {
+//	return func(t *Txn) {
+//		t.Description = description
+//	}
+//}
+//
+//func WithToAccount(toAccount *string) TxnOption {
+//	return func(t *Txn) {
+//		t.ToAccount = toAccount
+//	}
+//}
+//
+//func WithFromAccount(fromAccount *string) TxnOption {
+//	return func(t *Txn) {
+//		t.FromAccount = fromAccount
+//	}
+//}
+//
+//func WithTags(tags ...Tag) TxnOption {
+//	return func(t *Txn) {
+//		var tagsStr []string
+//		for _, tag := range tags {
+//			tagsStr = append(tagsStr, tag.Name)
+//		}
+//		t.Tags = tagsStr
+//	}
+//}
+//
+//func WithAmount(amount float64) TxnOption {
+//	return func(t *Txn) {
+//		t.Amount = amount
+//	}
+//}
+//
+//func NewTxn(userID string, opts ...TxnOption) Txn {
+//	txn := Txn{Username: userID, ID: uuid.New().String()}
+//
+//	for _, opt := range opts {
+//		opt(&txn)
+//	}
+//	return txn
+//}
